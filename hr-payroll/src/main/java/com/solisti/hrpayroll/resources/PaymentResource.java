3233df3460dacae4343156158744fb0ea04218d5
dgoncalves1 <Darly.Goncalves@fazenda.rj.gov.br>
2022-12-27 21:27:33 -0300

    Updates in payroll and hr-api-gateway created

--- hr-payroll/src/main/java/com/solisti/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/solisti/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.solisti.hrpayroll.entities.Payment;
 import com.solisti.hrpayroll.services.PaymentService;
 
@@ -17,9 +18,22 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> payment(@PathVariable("workerId")  Long workerId, @PathVariable("days") Long days){
 		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Long days){
+		
+//		try {
+//		Thread.sleep(10000L);
+//		}catch (Exception e) {
+//			// TODO: handle exception
+//		}
+		
+		Payment payment = new Payment("Darly", 300.0, days);
 		return ResponseEntity.ok(payment);
 	}
 	
