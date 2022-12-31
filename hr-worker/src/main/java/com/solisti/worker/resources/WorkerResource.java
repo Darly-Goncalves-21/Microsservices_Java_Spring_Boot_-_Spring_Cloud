29daf9cfc77e214ad673072cc43cffc262d04092
dgoncalves1 <Darly.Goncalves@fazenda.rj.gov.br>
2022-12-31 11:56:47 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/com/solisti/worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/solisti/worker/resources/WorkerResource.java
@@ -7,8 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.solisti.worker.entities.Worker;
 import com.solisti.worker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
